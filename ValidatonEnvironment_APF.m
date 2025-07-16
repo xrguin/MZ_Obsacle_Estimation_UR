f9a592f5d4d8375b1897a3bb1944e7eedbd05b25
xiangxiangliuwo <zzzzyguo@163.com>
2025-07-17 03:51:47 +0800

    update

--- ValidatonEnvironment_APF.m
+++ ValidatonEnvironment_APF.m
@@ -102,6 +102,37 @@ while n <= samples
         robot1.CurrentCoord = robot1.CurrentPose(1:2);
         robot1.DetectionRadius = 15;
 
+            % === Generate second ego robot: robot4 ===
+        robot4 = MbRobot;
+        robot4.name = 'Ego2';
+
+        [robot4.Start, robot4.Goal] = generateEgoRobotPatha(robot1.Start,robot2.Start, robot3.Start, ...
+            interceptPoint,test_site_size,robot3.SafeRadius);
+
+        % 保证robot4的起点不与robot1、robot2、robot3重合
+      %  existing_positions = [robot1.Start; robot2.Start; robot3.Start];
+      %  while true
+      %      candidateStart = rand(1,2)*test_site_size;
+      %      if all(vecnorm(existing_positions - candidateStart, 2, 2) > robot1.SafeRadius)
+      %          break;
+       %     end
+        %end
+
+        % 生成一个足够远的goal
+       % robot4.Start = candidateStart;
+       % robot4.Goal = generateRandomCoords(1, robot4.Start, distance, test_site_size);
+
+        lightColor1 = [255, 178, 102]/255 ; 
+        robot4.LightColor = lightColor1;
+        robot4.DarkColor = lightColor1;
+        robot4.SafeRadius = r_a;
+        robot4.HeadAngle = atan2(robot4.Goal(2) - robot4.Start(2), ...
+                              robot4.Goal(1) - robot4.Start(1));
+        robot4.CurrentPose = [robot4.Start'; robot4.HeadAngle; robot4.AngularVelocity];
+        robot4.CurrentCoord = robot4.CurrentPose(1:2);
+        robot4.DetectionRadius = 15;
+
+
         %==============================================================================
         collision_conditions_mz{n,1} = robot1.Start;
         collision_conditions_mz{n,2} = robot1.Goal;
@@ -109,9 +140,11 @@ while n <= samples
         collision_conditions_mz{n,4} = robot2.Goal;
         collision_conditions_mz{n,5} = robot3.Start;
         collision_conditions_mz{n,6} = robot3.Goal;
+        collision_conditions_mz{n,7} = robot4.Start;
+        collision_conditions_mz{n,8} = robot4.Goal;
     
         % Record if ego robot has collided with any other agnet
-        collision_conditions_mz{n,7} = 0;
+        collision_conditions_mz{n,9} = 0;
         % % Record the average prediction error 
         % collision_conditions_mz{n,8} = 0;
 %%
@@ -120,6 +153,7 @@ while n <= samples
         poses1 = [];
         poses2 = [];
         poses3 = [];
+        poses4 = [];
 %%
         % Define sample time, and simulation time
         sampleTime = 0.1;
@@ -140,6 +174,7 @@ while n <= samples
             robot1Pose = robot1.CurrentPose;  
             robot2Pose = robot2.CurrentPose;
             robot3Pose = robot3.CurrentPose;
+            robot4Pose = robot4.CurrentPose;
             if t == sampleTime
                 robot2Pose(4) = 0;
                 robot3Pose(4) = 0;
@@ -147,8 +182,9 @@ while n <= samples
             poses2 = [poses2, [robot2Pose; t]];
             poses3 = [poses3, [robot3Pose; t]];
             poses1 = [poses1, [robot1Pose; t]];
+            poses4 = [poses4, [robot4Pose; t]];
             
-            obstaclePose2 = [robot3Pose(1:2,:)];
+            obstaclePose2 = [robot3Pose(1:2,:),robot4Pose(1:2,:),robot1Pose(1:2,:)];
             obstaclePose3 = [robot2Pose(1:2,:)];
             robot2.Obstacle = obstaclePose2;
             
@@ -195,21 +231,35 @@ while n <= samples
 
             % obstaclePosAll = estmRobot3;
             % obstaclePosAll = [robot2Pose(1:2,:)];
-            robot1.Obstacle = obstaclePosAll;
+            robot1.Obstacle = [obstaclePosAll, robot4Pose(1:2,:)];
             robot1.artificial_potential_field(sampleTime, 1, 1.2);
+            robot4.Obstacle = [obstaclePosAll, robot1Pose(1:2,:)];
+            robot4.artificial_potential_field(sampleTime, 1, 1.2);
             
             if calcDist(robot1.CurrentCoord, robot2.CurrentCoord) < robot1.SafeRadius ||...
-                calcDist(robot1.CurrentCoord, robot3.CurrentCoord) < robot1.SafeRadius
-                collision_conditions_mz{n,7} = 1;
+                calcDist(robot1.CurrentCoord, robot3.CurrentCoord) < robot1.SafeRadius ||...
+                calcDist(robot1.CurrentCoord, robot4.CurrentCoord) < robot1.SafeRadius ||...
+                calcDist(robot4.CurrentCoord, robot2.CurrentCoord) < robot4.SafeRadius || ...
+                calcDist(robot4.CurrentCoord, robot1.CurrentCoord) < robot4.SafeRadius || ...
+                calcDist(robot4.CurrentCoord, robot3.CurrentCoord) < robot4.SafeRadius
+                collision_conditions_mz{n,9} = 1;
                 break;
             end
             % If any agent reaches the goal then the simulation stops
-            if robot1.atGoal()
+            if robot1.atGoal() && robot4.atGoal() 
                 break;
             elseif robot2.atGoal()
                 robot2.LinearVelocity = 0;
+                robot2.AngularVelocity = 0;
             elseif robot3.atGoal()
                 robot3.LinearVelocity = 0;
+                robot3.AngularVelocity = 0;
+            elseif robot1.atGoal() && ~robot4.atGoal()
+                robot1.LinearVelocity = 0;
+                robot1.AngularVelocity = 0;
+            elseif robot4.atGoal() && ~robot1.atGoal()
+                robot4.LinearVelocity = 0;
+                robot4.AngularVelocity = 0;
             end
             
 %%
@@ -221,6 +271,7 @@ while n <= samples
                 robot2.plotRobot(1); % adjust the frame size as needed
                 robot3.plotRobot(1);
                 robot1.plotRobot(1);
+                robot4.plotRobot(1);
         
                 hold on
                 % plot(robot1.Start(1),robot1.Start(2),'go','MarkerSize',10,'MarkerFaceColor','g')
@@ -230,19 +281,21 @@ while n <= samples
                 %     'Color',[1,50,32]/255,'LineWidth',1);   % Orange: [1 0.5 0]
                 scatter(poses2(1,1:2:end),poses2(2,1:2:end),800,robot2.LightColor(1:3));
                 scatter(poses3(1,1:2:end),poses3(2,1:2:end),800,robot3.LightColor(1:3));
+                scatter(poses4(1,1:2:end),poses4(2,1:2:end),800,robot4.LightColor);
                 % plot(poses3(1,:),poses3(2,:),'-',...
                 %     'Color',[0.5,0.5,0.5],'LineWidth',1);
                 try
                     % plot(estm_plot(1,:),estm_plot(2,:),'--',...
                     %     'Color',[0 0 1],'LineWidth',1)
                     
-                    if calcDist(estmRobot3, robot3.CurrentCoord) < robot1.SafeRadius
+                    if calcDist(estmRobot3, robot3.CurrentCoord) < robot1.SafeRadius || calcDist(estmRobot3, robot3.CurrentCoord) < robot4.SafeRadius
                         estm_pos3 = [estm_pos3, estmRobot3];
                         % scatter(estmRobot3(1), estmRobot3(2),120,'blue','square','filled')
                         % scatter(estm_pos3(1,1:2:end),estm_pos3(2,1:2:end),800,'blue')
                     % else
                         % scatter(estmRobot3(1), estmRobot3(2),120,'cyan','square','filled')
                     end
+
                 catch 
                     
                 end
@@ -298,3 +351,31 @@ collision_rate_mz = num_collide_mz / samples
 % poses3InWindow = poses3(:,timeIndices);
 % prediction_error = poses3InWindow(1:2,:) - estm_plot(1:2,:);
 % avg_error = mean(sqrt(prediction_error(1,:).^2 + prediction_error(2,:).^2))
+function [egoStart, egoGoal] = generateEgoRobotPatha(partnerStart, allyStart, enemyStart, interceptPoint, test_site_size, safeRadius)
+    max_attempts = 10000;
+    for attempt = 1:max_attempts
+        % Calculate distance from robot2Start to interceptPoint
+        distance = calcDist(interceptPoint, allyStart);
+        
+        vect_to_intercept = interceptPoint - partnerStart;
+        vect_to_intercept = vect_to_intercept / norm(vect_to_intercept);
+        
+        angle = -pi * rand();
+        egoStart = partnerStart + 2 * distance * vect_to_intercept * ...
+            [cos(angle), -sin(angle); sin(angle), cos(angle)];
+        
+        
+        % Check if egoStart is far enough from both robot2Start and robot3Start
+        if norm(egoStart - allyStart) > safeRadius && norm(egoStart - enemyStart) > safeRadius && norm(egoStart-partnerStart) > safeRadius
+            % Calculate egoGoal
+            dir = interceptPoint - egoStart;
+            egoGoal = interceptPoint + dir;
+            
+            % Check if egoGoal is within the test site
+            if all(egoGoal >= 0 & egoGoal <= test_site_size)
+                return
+            end
+        end
+    end
+    error('Unable to find a suitable configuration for ego robot after %d attempts', max_attempts);
+end
